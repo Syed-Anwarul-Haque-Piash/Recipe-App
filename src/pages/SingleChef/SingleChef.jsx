3f73e6523072530e7bb358b9db6bda7635d615ff
Syed-Anwarul-Haque-Piash <syed88783@gmail.com>
2023-05-02 22:25:56 +0600

    card and table data

--- src/pages/SingleChef/SingleChef.jsx
+++ src/pages/SingleChef/SingleChef.jsx
@@ -1,21 +1,83 @@
 import React from "react";
-import { Card, Container } from "react-bootstrap";
+import { Button, Card, Container, Table } from "react-bootstrap";
 import { useLoaderData, useParams } from "react-router-dom";
+import { FaEye, FaRegBookmark, FaRegStar, FaShareAlt, FaStar } from "react-icons/fa";
+import Rating from "react-rating";
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
 
 const SingleChef = () => {
   const chef = useLoaderData();
+  //console.log(chef.recipe[0].ingredients);
   console.log(chef);
+  const { chefName, id, bio, like, number, img, recipe, experience } = chef;
   // const id=useParams();
   // console.log(id)
   return (
     <Container>
       <Card className="mt-5">
-        <Card.Img variant="top" style={{height:"500px",width:"100%"}} src={chef.img} />
-        <Card.Body>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+        <Card.Img
+          variant="top"
+          style={{ height: "500px", width: "100%" }}
+          src={img}
+        />
+        <Card.Body className="text-secondary">
+          <Card.Text className="text-dark">
+            <h1>{chefName}</h1>
           </Card.Text>
+          <Card.Text>
+            <h3>{bio}</h3>
+          </Card.Text>
+          <Card.Text>
+            <h3>{like} people like this</h3>
+          </Card.Text>
+          <Card.Text>
+            <h3>{number} of item cooked</h3>
+          </Card.Text>
+          <Card.Text>
+            <h3>{experience} years experienced</h3>
+          </Card.Text>
+
+          {recipe.map((r) => (
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Recipe Name</th>
+                  <th colSpan={5}>{r.name}</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th>Ingredients</th>
+                  {r.ingredients.map((i) => (
+                    <td>{i}</td>
+                  ))}
+                </tr>
+                <tr>
+                  <th>Cooking Method</th>
+                  {r.steps.map((s) => (
+                    <td>{s}</td>
+                  ))}
+                </tr>
+                <tr>
+                  <th >Rating</th>
+                  <td colSpan={5}>
+                    {r.rating}
+                    <Rating
+                      placeholderRating={r.rating}
+                      readonly
+                      emptySymbol={<FaRegStar></FaRegStar>}
+                      placeholderSymbol={
+                        <FaStar className="text-warning"></FaStar>
+                      }
+                      fullSymbol={<FaStar></FaStar>}
+                    ></Rating>
+                  </td>
+                </tr>
+              </tbody>
+            </Table>
+          ))}
+           <Button  variant="success">Favourite</Button>
         </Card.Body>
       </Card>
     </Container>
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -23,5 +23,6 @@ const router = createBrowserRouter([
         ]
     },
     
+    
 ]);
 export default router;
